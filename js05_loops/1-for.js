e41d938ee9d94ccde68cf7ca3ffe7fa02ccb1f9b
fadime <fdimakdag@gmail.com>
2022-08-17 22:01:52 +0300

    derss5

---
+++ js05_loops/1-for.js
@@ -0,0 +1,22 @@
+// * ============================================
+// *            DONGULER(LOOPS)- FOR
+// * ============================================
+
+console.log("****** FOR *******");
+//? Ornek: 1 den n kadar sayiları toplayan kodu yaziniz.
+
+
+//? Ornek: 0-100 Arasinda 10 adet rasgele sayi ureten kodu
+//? for dongusu ile yaziniz..
+
+//* NOT:
+//* Math.floor() => en yakın alt tam sayıya (surekli asagiya) yuvarlar
+//* Math.ceil() => en yakın üst tam sayıya (sürekli yukariya) yuvarlar
+//* Math.round() => degerine gore yuvarlar..5 teb-n sonra yukarı digeri aşagı
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
+//*Math.random() 0 ile 0.99 arasinda sayi üretir
+
+
+//? ÖRNEK: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
+//? for dongulerini kullanarak yaziniz.
