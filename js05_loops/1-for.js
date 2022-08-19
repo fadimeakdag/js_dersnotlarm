c5d2a0ec10d43580243d88d005994e636c7ea8f0
fadime <fdimakdag@gmail.com>
2022-08-19 17:07:29 +0300

    ders-2

--- js05_loops/1-for.js
+++ js05_loops/1-for.js
@@ -3,20 +3,66 @@
 // * ============================================
 
 console.log("****** FOR *******");
-//? Ornek: 1 den n kadar sayiları toplayan kodu yaziniz.
+
+//?konsola 10 kere bir ifade yazdıran program yazınız
+
+for(let i=1 ; i<=10; i++){
+    console.log("hello");
+}
+
+for(let i=1 ; i<=100; i++){
+    console.log(`${i}-hello`);
+}
+
+//? Ornek: 1 den n kadar sayiları toplayan kodu yazınız.
 
 
-//? Ornek: 0-100 Arasinda 10 adet rasgele sayi ureten kodu
+const n= Number(prompt("bir sayı giriniz")) 
+let toplam=0 // globalde çalışması için buraya verdik degeri
+for (let i=1; i<=n; i++){
+    // toplam = toplam+i
+    toplam +=i
+    console.log(toplam, i)
+}
+console.log("SONUC:", toplam)
+
+
+//? Ornek: 0-100 Arasinda 10 adet rasgele tamsayi ureten kodu
 //? for dongusu ile yaziniz..
 
-//* NOT:
-//* Math.floor() => en yakın alt tam sayıya (surekli asagiya) yuvarlar
-//* Math.ceil() => en yakın üst tam sayıya (sürekli yukariya) yuvarlar
-//* Math.round() => degerine gore yuvarlar..5 teb-n sonra yukarı digeri aşagı
-//* Math.trunc() => kesirli sayinin tam kismini alir.
+//* NOT: yuvarlam fonksiyonları
+//* Math.floor() => en yakın alt tam sayıya (surekli asagiya) yuvarlar(21.1 21 yapar 21.9 21 yapar)// bunda hep asağıya yuvarlar 100 çıkmaz hiç
+//* Math.ceil() => en yakın üst tam sayıya (sürekli yukariya) yuvarlar(21.1 i 22 yapar 21.9 u 22 yapar)(0 hiç çıkmaz )
+//* Math.round() => degerine gore yuvarlar..5 teb-n sonra yukarı digeri aşagı(45.5 46 yapar 45.4  45 yapar buçuk ve üstü üstüne yuvarar)
+// round yaparsak 0 ile 100 arası dediğimiz için üretir o ile 100
+//* Math.trunc() => kesirli sayinin tam kismini alir. yuvarlamaya (virgüle bakmaz)
 
-//*Math.random() 0 ile 0.99 arasinda sayi üretir
+//*Math.random() 0 ile 0.99 arasinda sayi üretir.küsüratlı sayı
+
+for( let i=1; i<=10; i++) {
+    const rastgele = Math.round(Math.random() *100 ) ;//100 grubuna getiriyoruz kusuratlıları 100 e kadar istedi 100 le çarpıyoruz
+    console.log(rastgele);
+    
+}
 
 
 //? ÖRNEK: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
 //? for dongulerini kullanarak yaziniz.
+
+const sayı = +prompt("Pozitif bir sayı giriniz:");
+let asal = true; // flek mantığı bayrak boolean değişkene başlangıç değişkeni veriyoruz o değişkeni aşagıda kontrol ediyoruz .durum değişikliği olduğu yerde folslıyoruz
+if(sayı <= 1){
+    alert("Sayı 1'den büyük olmalıdır");
+} //negatif 0 ve 1 i girmesin diye alertle uyarı verdik asal sayı 1 ve kedniden başka böleni olnayan olduğu için 1 i aradsn çıkardır
+else{
+    for(let i = 2 ; i<sayı; i++){
+       if(sayı % i  === 0) {
+        asal = false;
+        break;
+       } 
+    }
+   
+const sonuc = asal == true ? "ASALDIR" : "ASAL DEĞİLDİR"; // burayı asal veya deil mi diye yyazdırmak için değişken atadık yukarıda  asal true dedik
+console.log(`${sayı} ${sonuc}`);
+     
+}// for döngüsünü neden kuruyoruz girilen sayı kadar bölünebilirliği kontrol etsin diye
--- js05_loops/while-dowhile.js
+++ js05_loops/while-dowhile.js
@@ -4,30 +4,73 @@
 
 console.log("****** WHILE *******");
 
+//?10 kere konsola merhaba yazdıran uygulamanın while döngüsü ile yazımı
+
+// * let i = 1; //dışarda sayaç olmalı değişken yani içerdede o değişken arttırılmalı
+// * while (i<=10) {
+// *    console.log(i + "  merhaba");
+//  *   i++;//i yi arttırmazsan değer sabit kalır ve sonsuz döngüye girer
+// * }
+
+
+
+
+
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu while dongusu ile yaziniz.
+//if le tekrar bakamayız yanlış girdiğinde bu yüzden while kullanoyoruz
+
+//if (not < 0 // not<100){
+    //alert("not 0 ile 100 arasında olmalıdır")
+//}
+
+//* let not = prompt("Lütfen 0-100 arasıda not giriniz.");
+//* while (not < 0 || not > 100){
+// *    console.log("Not 0-100 arası olmalıdır");
+// *    not = prompt("Lütfen 0-100 arası bir not girin.");
+//* }
+//* console.log("Girilen not: ", not)
+
+
+console.log("*******DO-WHİLE*******")
+//?10 kere konsolda merhaba yazdıran uygulama do while dongüsüs ile yapın
+let sayac =1;
+do {
+    console.log("merhaba -", sayac);
+    sayac++;
+} while (sayac <= 10);
 //? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
 //? not girmeye zorlayan kodu while dongusu ile yaziniz.
 
+ let not1; // not u tanımladık
+ do{
+   not1 = +prompt("Lütfen 0-100 arasıda not giriniz."); if (not1 < 0 || not1 > 100){
+    console.log("not 0-100 arası olmalıdır.")
+   }
+ }while (not1 < 0 || not1 > 100);
+ console.log("Girilen not: ", not1)
 
 
 
 
-let pc= Math.trunc(Math.random()*100);
-console.log(pc);
-for(let i=1 ; i<=6 ; i++){
-        let tahmin = +prompt(`tahmininiz:`);
-        if(tahmin===pc){
-            console.log(`bildiniz`);
-            break
-        }
-        else{
-            if(tahmin<pc){
-                console.log(`arttırın`);
-                console.log( (5-i)+ ` hakkınız kaldı`);
-            }
-            else{
-                console.log(`azaltın`);
-                console.log( (5-i)+ ` hakkınız kaldı`);
-            }
-        }
-}
+// let pc= Math.trunc(Math.random()*100);
+// console.log(pc);
+// for(let i=1 ; i<=6 ; i++){
+//         let tahmin = +prompt(`tahmininiz:`);
+//         if(tahmin===pc){
+//             console.log(`bildiniz`);
+//             break
+//         }
+//         else{
+//             if(tahmin<pc){
+//                 console.log(`arttırın`);
+//                 console.log( (5-i)+ ` hakkınız kaldı`);
+//             }
+//             else{
+//                 console.log(`azaltın`);
+//                 console.log( (5-i)+ ` hakkınız kaldı`);
+//             }
+//         }
+// }
